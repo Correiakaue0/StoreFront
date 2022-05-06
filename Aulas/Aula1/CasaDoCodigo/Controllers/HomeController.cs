1c54d19b9ee7d1024f10d628fb320224c73c1fae
kaue <correiakaue0@gmail.com>
2022-05-06 11:17:53 -0300

    FirstCommit

---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        //DAQUI PRA BAIXO NAO IMPORTA
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
